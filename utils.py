848b34129b4c3dc408313cfa23ede438bbe8cb2a
AlexLaptop <alexmess.carrier@gmail.com>
2025-04-14 10:04:24 +0200

    Removed OpenAI due to being not free to use.

---
+++ utils.py
@@ -0,0 +1,8 @@
+from langchain.document_loaders import PyPDFLoader, TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+import os
+import tempfile
