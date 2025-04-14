25a8beeb5af9accd8a729b29317ea8bba0c0c679
AlexLaptop <alexmess.carrier@gmail.com>
2025-04-14 10:21:27 +0200

    Utils finished

--- utils.py
+++ utils.py
@@ -1,8 +1,42 @@
-from langchain.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from transformers import pipeline
+from sentence_transformers import SentenceTransformer
 from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
+from langchain.schema import Document
+from langchain.text_splitter import CharacterTextSplitter
 import os
 import tempfile
+
+
+qa_model = pipeline("Question-answering", model="distilbert-base-uncased-distilled-squad")
+embedder = SentenceTransformer("all-MiniLM-L6-v2")
+
+def process_document(uploaded_file):
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.read())
+        tmp_path = tmp_file.name
+
+    if uploaded_file.name.endswith(".pdf"):
+        from langchain.document_loaders import PyPDFLoader
+        loader = PyPDFLoader(tmp_path)
+    else:
+        from langchain.document_loaders import TextLoader
+        loader = TextLoader(tmp_file)
+    
+    raw_docs = loader.load()
+    splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    docs = splitter.split_documents(raw_docs)
+    return docs
+
+def ask_question(docs, question):
+    texts = [doc.page_content for doc in docs ]
+    embeddings = embedder.encode(texts)
+
+    from sklearn.metrics.pairwise import cosine_similarity
+    question_embedding = embedder.encode([question])
+    similarities = cosine_similarity(question_embedding, embeddings)[0]
+
+    top_index =similarities.argmax()
+    context = texts[top_index]
+
+    result = qa_model(question=question, context=context)
+    return result["answer"]
